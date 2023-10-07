458b68c03c13b20bd68d3aba67ffb9ac7cf49ace
Richard Gladman <gitlab@thefifthcontinent.com>
2023-10-07 11:53:32 +0100

    Currency delegate now uses symbol from settings

--- delegates/currencydelegate.cpp
+++ delegates/currencydelegate.cpp
@@ -1,6 +1,7 @@
 #include "currencydelegate.h"
 
 #include <QPainter>
+#include "../settings/settings.h"
 
 CurrencyDelegate::CurrencyDelegate(QObject *parent) : QStyledItemDelegate{parent}
 {
@@ -10,14 +11,17 @@ CurrencyDelegate::CurrencyDelegate(QObject *parent) : QStyledItemDelegate{parent
 
 void CurrencyDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
+    extern Settings *settings;
+
     if (index.column() != column) {
         QStyledItemDelegate::paint(painter, option, index);
         return;
     }
 
     double amount = index.data().toDouble();
+    QString text = settings->getCurrency() + QString::number(amount, 'f', 2);
 
-    painter->drawText(option.rect, Qt::AlignVCenter, QString::number(amount, 'f', 2));
+    painter->drawText(option.rect, Qt::AlignVCenter, text);
 }
 
 int CurrencyDelegate::getColumn() const
