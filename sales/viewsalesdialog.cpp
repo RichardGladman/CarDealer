53a1b27b1ab1eca269551614d6eea7e8fdcffa92
Richard Gladman <gitlab@thefifthcontinent.com>
2023-10-07 12:07:21 +0100

    View sale now uses symbol from settings

--- sales/viewsalesdialog.cpp
+++ sales/viewsalesdialog.cpp
@@ -9,10 +9,14 @@
 #include "../vehicles/vehicle.h"
 #include "../sellers/seller.h"
 
+#include "../settings/settings.h"
+
 ViewSalesDialog::ViewSalesDialog(int id, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ViewSalesDialog)
 {
+    extern Settings *settings;
+
     ui->setupUi(this);
 
     SalesModel salesModel = SalesModel::load(id);
@@ -27,8 +31,8 @@ ViewSalesDialog::ViewSalesDialog(int id, QWidget *parent) :
     ui->labelVehicleManafacturer->setText(vehicle.getManufacturer());
     ui->labelVehicleMiles->setText(QString::number(vehicle.getMiles(), 'f', 2));
     ui->labelVehicleCondition->setText(vehicle.getVehicleCondition());
-    ui->labelVehiclePrice->setText(vehicle.getCurrency() + " " + QString::number(vehicle.getPrice(), 'f', 2));
-    ui->labelNegotiatedPrice->setText(vehicle.getCurrency() + " " + QString::number(salesModel.getNegotiatedPrice(), 'f', 2));
+    ui->labelVehiclePrice->setText(settings->getCurrency() + " " + QString::number(vehicle.getPrice(), 'f', 2));
+    ui->labelNegotiatedPrice->setText(settings->getCurrency() + " " + QString::number(salesModel.getNegotiatedPrice(), 'f', 2));
     ui->labelSaleDate->setText(salesModel.getAddedDate());
     ui->labelSellerName->setText(seller.getFirstName() + " " + seller.getLastName());
     ui->labelSellerEmail->setText(seller.getEmail());
